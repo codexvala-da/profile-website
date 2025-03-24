06a70b54f73cf885402e391b51fe6c6463b40788
codexvala-da <vala.da@northeastern.edu>
2025-03-24 19:46:52 -0400

    speed insights added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,13 @@ import ProjectsSection from "./components/ProjectsSection";
 import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <Analytics />
+      <SpeedInsights />
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
