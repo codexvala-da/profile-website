eab2e3e0002e5cdf445313e9ba3b307848a0f5ad
codexvala-da <vala.da@northeastern.edu>
2025-03-24 21:35:51 -0400

    background updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,17 +10,36 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]">
+    <main className="flex min-h-screen flex-col bg-[#121212] relative">
       <Analytics />
       <SpeedInsights />
-      <Navbar />
-      <div className="container mt-24 mx-auto px-12 py-4">
-        <HeroSection />
-        <AboutSection />
-        <ProjectsSection />
-        <EmailSection />
+
+      {/* Video Background */}
+      <div className="absolute inset-0 overflow-hidden z-0">
+        <video
+          autoPlay
+          loop
+          muted
+          className="absolute top-1/2 left-1/2 min-w-full min-h-full transform -translate-x-1/2 -translate-y-1/2 object-cover"
+        >
+          <source src="/images/video.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        {/* Overlay to make video less distracting */}
+        <div className="absolute inset-0 bg-black opacity-50"></div>
       </div>
-      <Footer />
+
+      {/* Content with slightly higher z-index */}
+      <div className="relative z-10">
+        <Navbar />
+        <div className="container mt-24 mx-auto px-12 py-4">
+          <HeroSection />
+          <AboutSection />
+          <ProjectsSection />
+          <EmailSection />
+        </div>
+        <Footer />
+      </div>
     </main>
   );
 }
